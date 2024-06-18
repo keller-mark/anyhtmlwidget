b588447706964d8df36632b2f984c356ed760dcf
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-06-18 06:25:43 -0600

    Prefix internal fields (#22)
    * Prefixes
    
    * ignore things
    
    * Vignette cleanup

--- R/widget.R
+++ R/widget.R
@@ -74,13 +74,13 @@ AnyHtmlWidget <- R6::R6Class("AnyHtmlWidget",
     # TODO: prefix with dot
     # since R6 requires all items in
     # public, private, and active to have unique names
-    esm = NULL,
-    values = NULL,
-    mode = NULL,
-    change_handler = NULL,
-    server = NULL,
-    server_host = NULL,
-    server_port = NULL,
+    .esm = NULL,
+    .values = NULL,
+    .mode = NULL,
+    .change_handler = NULL,
+    .server = NULL,
+    .server_host = NULL,
+    .server_port = NULL,
     .width = NULL,
     .height = NULL
   ),
@@ -95,8 +95,8 @@ AnyHtmlWidget <- R6::R6Class("AnyHtmlWidget",
     #' @param .commands TODO
     #' @param ... All other named arguments will be used to create active bindings on the instance.
     initialize = function(.esm, .mode, .width = NA, .height = NA, .commands = NA, ...) {
-      private$esm <- .esm
-      private$values <- list(...)
+      private$.esm <- .esm
+      private$.values <- list(...)
 
       if(is.na(.width)) {
         .width <- "100%"
@@ -108,24 +108,24 @@ AnyHtmlWidget <- R6::R6Class("AnyHtmlWidget",
       private$.width <- .width
       private$.height <- .height
 
-      private$server_host <- "0.0.0.0"
-      private$server_port <- httpuv::randomPort(min = 8000, max = 9000, n = 1000)
+      private$.server_host <- "0.0.0.0"
+      private$.server_port <- httpuv::randomPort(min = 8000, max = 9000, n = 1000)
 
       if(!.mode %in% c("static", "gadget", "shiny", "dynamic")) {
         stop("Invalid widget mode.")
       }
-      private$mode <- .mode
+      private$.mode <- .mode
 
       active_env <- self$`.__enclos_env__`$`.__active__`
 
       # TODO: check that values is not NA
-      for(key in names(private$values)) {
+      for(key in names(private$.values)) {
         active_binding <- function(val) {
           if(missing(val)) {
-            return(self$get_value(key))
+            return(self$.get_value(key))
           } else {
-            self$set_value(key, val)
-            if(private$mode == "static") {
+            self$.set_value(key, val)
+            if(private$.mode == "static") {
               self$print()
             }
           }
@@ -139,98 +139,98 @@ AnyHtmlWidget <- R6::R6Class("AnyHtmlWidget",
     #' Set a value.
     #' @param key The key of the value to set.
     #' @param val The new value.
-    #' @param emit_change Should the on_change handler be called?
-    set_value = function(key, val, emit_change = TRUE) {
-      private$values[[key]] <- val
-      if(emit_change && !is.null(private$change_handler)) {
+    #' @param emit_change Should the .on_change handler be called?
+    .set_value = function(key, val, emit_change = TRUE) {
+      private$.values[[key]] <- val
+      if(emit_change && !is.null(private$.change_handler)) {
         # Should this only call the callback if the current value is different than the new value?
-        private$change_handler(key, val)
+        private$.change_handler(key, val)
       }
     },
     #' @description
-    #' Register a change handler to call if emit_change is TRUE in set_value.
+    #' Register a change handler to call if emit_change is TRUE in .set_value.
     #' @param callback A callback function to register.
-    on_change = function(callback) {
-      private$change_handler <- callback
+    .on_change = function(callback) {
+      private$.change_handler <- callback
     },
     #' @description
     #' Get a particular value.
     #' @param key The key of the value to get.
     #' @returns The value.
-    get_value = function(key) {
-      return(private$values[[key]])
+   .get_value = function(key) {
+      return(private$.values[[key]])
     },
     #' @description
     #' Get the ESM string.
     #' @returns The ESM string.
-    get_esm = function() {
-      return(private$esm)
+    .get_esm = function() {
+      return(private$.esm)
     },
     #' @description
     #' Get all widget values
     #' @returns List of values.
-    get_values = function() {
-      return(private$values)
+    .get_values = function() {
+      return(private$.values)
     },
     #' @description
     #' Get the widget width.
     #' @returns The width.
-    get_width = function() {
+    .get_width = function() {
       return(private$.width)
     },
     #' @description
     #' Get the widget height.
     #' @returns The height.
-    get_height = function() {
+    .get_height = function() {
       return(private$.height)
     },
     #' @description
     #' Set all values. TODO: is this ever used?
     #' @param new_values A list of new values.
-    set_values = function(new_values) {
-      private$values <- new_values
+    .set_values = function(new_values) {
+      private$.values <- new_values
     },
     #' @description
     #' Set the widget mode.
     #' @param mode The new widget mode.
-    set_mode = function(mode) {
+    .set_mode = function(mode) {
       if(!mode %in% c("static", "gadget", "shiny", "dynamic")) {
         stop("Invalid widget mode.")
       }
-      private$mode <- mode
+      private$.mode <- mode
     },
     #' @description
     #' Start the server, if not running.
-    start_server = function() {
-      if(is.null(private$server)) {
-        private$server <- start_server(self, host = private$server_host, port = private$server_port)
+    .start_server = function() {
+      if(is.null(private$.server)) {
+        private$.server <- start_server(self, host = private$.server_host, port = private$.server_port)
       }
     },
     #' @description
     #' Stop the server, if running.
-    stop_server = function() {
-      if(!is.null(private$server)) {
-        private$server$stop()
+    .stop_server = function() {
+      if(!is.null(private$.server)) {
+        private$.server$stop()
       }
     },
     #' @description
     #' Get the server hostname.
     #' @return The hostname as a string.
-    get_host = function() {
-      return(private$server_host)
+    .get_host = function() {
+      return(private$.server_host)
     },
     #' @description
     #' Get the server port.
     #' @returns The port number.
-    get_port = function() {
-      return(private$server_port)
+    .get_port = function() {
+      return(private$.server_port)
     },
     #' @description
     #' Custom print function for the R6 class.
     #' If mode is "shiny", falls back to original R6 print behavior.
     #' Otherwise, renders the widget.
     print = function() {
-      if(private$mode == "shiny") {
+      if(private$.mode == "shiny") {
         # If Shiny mode, we just want to use the original R6 print behavior.
         # Reference: https://github.com/r-lib/R6/blob/507867875fdeaffbe7f7038291256b798f6bb042/R/print.R#L35C5-L35C36
         print(cat(format(self), sep = "\n"))
@@ -242,11 +242,11 @@ AnyHtmlWidget <- R6::R6Class("AnyHtmlWidget",
     #' @description
     #' Render the widget.
     render = function() {
-      if(private$mode == "static") {
+      if(private$.mode == "static") {
         invoke_static(self)
-      } else if(private$mode == "gadget") {
+      } else if(private$.mode == "gadget") {
         invoke_gadget(self)
-      } else if(private$mode == "dynamic") {
+      } else if(private$.mode == "dynamic") {
         invoke_dynamic(self)
       } else {
         stop("render is meant for use with static, gadget, and dynamic modes")
@@ -258,24 +258,24 @@ AnyHtmlWidget <- R6::R6Class("AnyHtmlWidget",
 #' @keywords internal
 invoke_static <- function(w) {
   w <- the_anyhtmlwidget(
-    esm = w$get_esm(),
-    values = w$get_values(),
-    width = w$get_width(),
-    height = w$get_height()
+    esm = w$.get_esm(),
+    values = w$.get_values(),
+    width = w$.get_width(),
+    height = w$.get_height()
   )
   print(w)
 }
 
 #' @keywords internal
 invoke_dynamic <- function(w) {
-  w$start_server()
+  w$.start_server()
   w <- the_anyhtmlwidget(
-    esm = w$get_esm(),
-    values = w$get_values(),
-    width = w$get_width(),
-    height = w$get_height(),
-    port = w$get_port(),
-    host = w$get_host()
+    esm = w$.get_esm(),
+    values = w$.get_values(),
+    width = w$.get_width(),
+    height = w$.get_height(),
+    port = w$.get_port(),
+    host = w$.get_host()
   )
   print(w)
 }
@@ -292,14 +292,14 @@ invoke_gadget <- function(w) {
     shiny::observeEvent(input$anyhtmlwidget_on_save_changes, {
       # update values on w here
       for(key in names(input$anyhtmlwidget_on_save_changes)) {
-        w$set_value(key, input$anyhtmlwidget_on_save_changes[[key]], emit_change = FALSE)
+        w$.set_value(key, input$anyhtmlwidget_on_save_changes[[key]], emit_change = FALSE)
       }
       increment(increment() + 1)
     })
 
     # output$values <- renderPrint({
     #   increment()
-    #   w$get_values()
+    #   w$.get_values()
     # })
     #
     # observeEvent(input$go, {
@@ -307,12 +307,12 @@ invoke_gadget <- function(w) {
     #   increment(increment() + 1)
     # })
 
-    w$on_change(function(key, new_val) {
+    w$.on_change(function(key, new_val) {
       session$sendCustomMessage("anyhtmlwidget_on_change", list(key = key, value = new_val))
     })
 
     output$my_widget <- render_anyhtmlwidget(expr = {
-      the_anyhtmlwidget(esm = w$get_esm(), values = w$get_values(), width = w$get_width(), height = w$get_height())
+      the_anyhtmlwidget(esm = w$.get_esm(), values = w$.get_values(), width = w$.get_width(), height = w$.get_height())
     })
   }
 
@@ -343,14 +343,14 @@ widgetServer <- function(id, w) {
   shiny::moduleServer(
     id,
     function(input, output, session) {
-      initial_values <- w$get_values()
+      initial_values <- w$.get_values()
       rv <- do.call(shiny::reactiveValues, initial_values)
 
       shiny::observeEvent(input$anyhtmlwidget_on_save_changes, {
         # update values on w here
         for(key in names(input$anyhtmlwidget_on_save_changes)) {
           rv[[key]] <- input$anyhtmlwidget_on_save_changes[[key]]
-          w$set_value(key, input$anyhtmlwidget_on_save_changes[[key]], emit_change = FALSE)
+          w$.set_value(key, input$anyhtmlwidget_on_save_changes[[key]], emit_change = FALSE)
         }
       })
 
@@ -361,7 +361,7 @@ widgetServer <- function(id, w) {
       }
 
       output$widget <- render_anyhtmlwidget(expr = {
-        the_anyhtmlwidget(esm = w$get_esm(), values = initial_values,  width = w$get_width(), height = w$get_height(), ns_id = id)
+        the_anyhtmlwidget(esm = w$.get_esm(), values = initial_values, width = w$.get_width(), height = w$.get_height(), ns_id = id)
       })
 
       return(rv)
